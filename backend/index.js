0f58892f2c12df916e7fe287b6b0c73ae56752f8
Bintang Adesa <bintang1984@gmail.com>
2022-04-23 22:21:24 +0700

    Refactoring backend code

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,38 @@
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const connectDB = require("./config/db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
 const postRoutes = require("./routes/posts");
 
+const app = express();
+
 dotenv.config();
 
 //mongodb connection handshake
-mongoose.connect(
-  process.env.MONGO_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  function (err) {
-    if (err) throw err;
-    console.log("Successfully connected");
-  }
-);
+connectDB();
 
 //middleware
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("common"));
+
+if (process.env.NODE_ENV === "DEVELOPMENT") {
+  app.use(morgan("dev"));
+}
 
 //api routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 
-//server start at port 5000
-app.listen(5000, () => {
-  console.log("backend server is running!");
-});
+//server start at port FROM .env PORT=
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
